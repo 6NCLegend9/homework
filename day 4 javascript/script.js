076cbb5c5166abd02b2389d485b0544c3449c3ef
NCLegend <59505853+6NCLegend9@users.noreply.github.com>
2024-03-28 07:27:32 -0500

    Add files via upload

---
+++ day 4 javascript/script.js
@@ -0,0 +1,74 @@
+//#1 Print to console
+
+//console.log('Hello World');
+
+//#2 Variable declaration
+
+// Declare a variable called 'message' and assign a string value
+//const message = "Hello, world!";
+
+// Print the value of the 'message' variable to the console
+//console.log(message);
+
+//#3 Addion
+
+// Define two numbers
+// const num1 = 10;
+// const num2 = 20;
+
+// Calculate the sum
+// const sum = num1 + num2;
+
+// Print the result
+// console.log("The sum of", num1, "and", num2, "is", sum);
+
+//#4 String concatenation
+
+// const str1 = "Hello";
+// const str2 = "World";
+// const concatenatedString = str1 + " " + str2;
+// console.log(concatenatedString); 
+
+// Output: "Hello World"
+
+//#5 String length
+
+// const myString = "Hello, world!";
+// const stringLength = myString.length;
+// console.log("The length of the string is:", stringLength);
+
+// stringLength = 13;
+
+//#6 String uppercase version
+
+// const myString = "Hello, world!";
+// const upperCaseString = myString.toUpperCase();
+// console.log("Uppercase version:", upperCaseString);
+
+// Uppercase version: HELLO, WORLD!
+// Output: HELLO, WORLD!
+
+//#7 String loworcase version
+
+// const myString = "Hello, World!";
+// const lowerCaseString = myString.toLowerCase();
+// console.log("Lowercase version:", lowerCaseString);
+
+// Lowercase version: hello, world!
+// Output: hello, world!
+
+//#8 String substring
+ // Starts from index 2 (inclusive) to the end
+
+ // const result2 = text.substring(2); 
+// console.log(result2);
+
+// Output: "llo, world!"
+
+//#9 String replace
+
+// const originalString = "original READ ONLY";
+// const modifiedString = originalString.replace("READ ONLY", "READ WRITE");
+// console.log(modifiedString); 
+
+// Output: "original READ WRITE"
